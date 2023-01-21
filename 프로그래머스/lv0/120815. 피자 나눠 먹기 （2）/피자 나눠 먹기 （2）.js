dafb1b38990c1d43fc3bb6308ec97ca2698bf2d7
JESin10 <119720123+JESin10@users.noreply.github.com>
2023-01-21 21:43:06 +0900

    Update 피자 나눠 먹기 （2）.js

--- 프로그래머스/lv0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.js
+++ 프로그래머스/lv0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.js
@@ -2,7 +2,7 @@ function solution (n) {
     let pizza = 6;
     let result = 0;
     if(n%6==0){
-        result = n/6; // 1번이라면 1저장
+        result = n/6; 
     } else if(n%2==0){
         result = n/2;
     } else if(n%3==0){
@@ -16,7 +16,7 @@ function solution (n) {
 // function solution (n) {
 //     let pizza = 6;
 //     while(pizza % n !== 0) {
-//         pizza =+=6
+//         pizza +=6
 //     } 
-//     return pizzaBox/6;
+//     return pizza/6;
 // }
