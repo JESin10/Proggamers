c7aea70a0167d188263f8c73ffba05bf5ac9d70d
JESin10 <119720123+JESin10@users.noreply.github.com>
2023-01-24 15:45:43 +0900

    Update 개미 군단.js

--- 프로그래머스/lv0/120837. 개미 군단/개미 군단.js
+++ 프로그래머스/lv0/120837. 개미 군단/개미 군단.js
@@ -1,23 +1,3 @@
-// function solution(hp) {
-//     var answer = 0;
-//     let g_ant = 5;
-//     let m_ant = 3;
-//     let n_ant = 1;
-//     while(hp>0){
-//         if(hp>g_ant || hp==g_ant){
-//             hp=hp-g_ant;
-//             answer = answer+1 ;    
-//         } else if(hp>m_ant || hp==m_ant) {
-//             hp=hp-m_ant;
-//             answer = answer+1;
-//         } else if(hp>n_ant || hp==n_ant) {
-//             hp=hp-n_ant;
-//             answer = answer+1;
-//         }
-//     }
-//     return answer;
-// }
-
 function solution(hp) {
     var answer = 0;
     let g_ant = 5;
