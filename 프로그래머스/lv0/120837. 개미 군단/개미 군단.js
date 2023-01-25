c9290962ef776fdf1a50d8f4b4b15afa99bd6676
JESin10 <119720123+JESin10@users.noreply.github.com>
2023-01-25 21:48:41 +0900

    Update 개미 군단.js

--- 프로그래머스/lv0/120837. 개미 군단/개미 군단.js
+++ 프로그래머스/lv0/120837. 개미 군단/개미 군단.js
@@ -17,24 +17,27 @@
 //     }
 //     return answer;
 // }
-var solution=h=>a=~~(h/5)+~~(h%5/3)+h%5%3
 
-// function solution(hp) {
-//     var answer = 0;
-//     let g_ant = 5;
-//     let m_ant = 3;
-//     let n_ant = 1;
-//     while(hp>0){
-//         if(hp>=g_ant){
-//             hp=hp-g_ant;
-//             answer = answer+1 ;    
-//         } else if(hp>=m_ant) {
-//             hp=hp-m_ant;
-//             answer = answer+1;
-//         } else if(hp>=n_ant) {
-//             hp=hp-n_ant;
-//             answer = answer+1;
-//         }
-//     }
-//     return answer;
-// }
+
+// var solution=h=>a=~~(h/5)+~~(h%5/3)+h%5%3
+// 더블틸트연산자를 활용 math.floor와 동일한 기
+
+function solution(hp) {
+    var answer = 0;
+    let g_ant = 5;
+    let m_ant = 3;
+    let n_ant = 1;
+    while(hp>0){
+        if(hp>=g_ant){
+            hp=hp-g_ant;
+            answer = answer+1 ;    
+        } else if(hp>=m_ant) {
+            hp=hp-m_ant;
+            answer = answer+1;
+        } else if(hp>=n_ant) {
+            hp=hp-n_ant;
+            answer = answer+1;
+        }
+    }
+    return answer;
+}
