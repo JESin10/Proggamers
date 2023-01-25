94eb9d3856bf262e10d2d5bd1f09dc0029e40609
JESin10 <119720123+JESin10@users.noreply.github.com>
2023-01-25 21:46:51 +0900

    [level 0] Title: 개미 군단, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120837. 개미 군단/개미 군단.js
+++ 프로그래머스/lv0/120837. 개미 군단/개미 군단.js
@@ -1,19 +1,40 @@
-function solution(hp) {
-    var answer = 0;
-    let g_ant = 5;
-    let m_ant = 3;
-    let n_ant = 1;
-    while(hp>0){
-        if(hp>=g_ant){
-            hp=hp-g_ant;
-            answer = answer+1 ;    
-        } else if(hp>=m_ant) {
-            hp=hp-m_ant;
-            answer = answer+1;
-        } else if(hp>=n_ant) {
-            hp=hp-n_ant;
-            answer = answer+1;
-        }
-    }
-    return answer;
-}
+// function solution(hp) {
+//     var answer = 0;
+//     let g_ant = 5;
+//     let m_ant = 3;
+//     let n_ant = 1;
+//     while(hp>0){
+//         if(hp>g_ant || hp==g_ant){
+//             hp=hp-g_ant;
+//             answer = answer+1 ;    
+//         } else if(hp>m_ant || hp==m_ant) {
+//             hp=hp-m_ant;
+//             answer = answer+1;
+//         } else if(hp>n_ant || hp==n_ant) {
+//             hp=hp-n_ant;
+//             answer = answer+1;
+//         }
+//     }
+//     return answer;
+// }
+var solution=h=>a=~~(h/5)+~~(h%5/3)+h%5%3
+
+// function solution(hp) {
+//     var answer = 0;
+//     let g_ant = 5;
+//     let m_ant = 3;
+//     let n_ant = 1;
+//     while(hp>0){
+//         if(hp>=g_ant){
+//             hp=hp-g_ant;
+//             answer = answer+1 ;    
+//         } else if(hp>=m_ant) {
+//             hp=hp-m_ant;
+//             answer = answer+1;
+//         } else if(hp>=n_ant) {
+//             hp=hp-n_ant;
+//             answer = answer+1;
+//         }
+//     }
+//     return answer;
+// }
