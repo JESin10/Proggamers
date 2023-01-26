68aeb05ec8712b1712fe147b3ee0ebc836ff46c2
JESin10 <119720123+JESin10@users.noreply.github.com>
2023-01-26 23:17:16 +0900

    Update 자릿수 더하기.js

--- 프로그래머스/lv0/120906. 자릿수 더하기/자릿수 더하기.js
+++ 프로그래머스/lv0/120906. 자릿수 더하기/자릿수 더하기.js
@@ -1,6 +1,5 @@
 function solution(n) {
     var answer = 0;
-    //let num = String(n);
     let arr = Array.from(String(n));
     for(let i=0;i<arr.length;i++){
         answer += parseInt(arr[i]);
