758ffe7a129b64b46da2de71e144d833def56107
JESin10 <119720123+JESin10@users.noreply.github.com>
2023-01-27 17:09:44 +0900

    Update 자연수 뒤집어 배열로 만들기.js

--- 프로그래머스/lv1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.js
+++ 프로그래머스/lv1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.js
@@ -2,6 +2,7 @@ function solution(n) {
     // 풀어쓰기 (숫자->문자열.문자열->배열.뒤집기.배열의문자->숫자)
     // let answer = String(n).split('').reverse().map(function(num){return Number(num)});
     // return answer;
-
-    return String(n).split('').reverse().map(function(num){return Number(num)});
+    
+    //줄여쓰기
+    return String(n).split('').reverse().map(num=> Number(num));
 }
