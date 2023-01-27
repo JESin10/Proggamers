d26853e8482e788fa43ee6cc2a99cc5ff42a4eb4
JESin10 <119720123+JESin10@users.noreply.github.com>
2023-01-27 12:02:55 +0900

    [level 1] Title: 짝수와 홀수, Time: 0.02 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12937. 짝수와 홀수/짝수와 홀수.js
@@ -0,0 +1,10 @@
+// function solution(num) {
+//     var answer = '';
+//     if(num%2===0){answer = 'Even';}
+//     else {answer = 'Odd'};
+//     return answer;
+// }
+
+function solution(num) {
+    return (num%2===0)?'Even':'Odd';
+}
