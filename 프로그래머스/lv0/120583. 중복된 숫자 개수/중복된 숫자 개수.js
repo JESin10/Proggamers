8e8be33f93c776d0cdc6a4ddff969320a39549c4
JESin10 <119720123+JESin10@users.noreply.github.com>
2023-01-27 22:25:18 +0900

    Update 중복된 숫자 개수.js

--- 프로그래머스/lv0/120583. 중복된 숫자 개수/중복된 숫자 개수.js
+++ 프로그래머스/lv0/120583. 중복된 숫자 개수/중복된 숫자 개수.js
@@ -1,3 +1,4 @@
+// 풀어쓰기
 // function solution(array, n) {
 //     var answer = 0;
 //     for(let i=0;i<array.length;i++){
@@ -10,6 +11,8 @@
 //     return answer;
 // }
 
+
+//줄여쓰기
 function solution(array, n) {
     var answer = 0;
     for(let i=0;i<array.length;i++){
