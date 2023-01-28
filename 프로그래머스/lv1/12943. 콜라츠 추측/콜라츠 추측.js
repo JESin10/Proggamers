a886fc84dfbf00d4f3ba4b10aa22bd06550f9495
JESin10 <119720123+JESin10@users.noreply.github.com>
2023-01-28 16:17:53 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.js
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -1,29 +1,14 @@
-function solution(num) {
-  //num값을 받아주는 target변수 선언
-  let target = num;
+// function solution(num) {
+//   let cnt = 0;
 
-  if (num == 1) return 0;
-  let cnt = 0; // 반복 횟수
-  //1-1, 1-2 결과값을 넣어주는 배열을 빈배열을 만든다
-  let arr = [];
+//   while (num != 1 && cnt != 500) {
+//     num % 2 == 0 ? (num = num / 2) : (num = num * 3 + 1);
+//     cnt++;
+//   }
+//   return num == 1 ? cnt : -1;
+// }
 
-  //계속 반복해서 500에 제한을 둬야하니 while문
-  while (cnt < 500) {
-    
-    //target 1되면 배열 return
-    if (target == 1) return arr.length;
 
-    if (target % 2 == 0) {
-      target = target / 2;
-      arr.push(target);
-    } else {
-      target = target * 3 + 1;
-      arr.push(target);
-    }
-    cnt += 1;
-  }
-  //   console.log(arr);
-
-  // 500번 초과되면 while문 빠져나가게되서 -1 출력
-  return -1;
+function solution(num,count = 0) {
+    return num == 1 ? (count >= 500 ? -1 : count) : solution(num % 2 == 0 ? num / 2 : num * 3 + 1,++count);
 }
