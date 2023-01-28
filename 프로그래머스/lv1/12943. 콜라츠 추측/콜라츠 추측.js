0b9c15375a07eaa7bc8d3466acf928a32c551381
JESin10 <119720123+JESin10@users.noreply.github.com>
2023-01-28 16:22:01 +0900

    Update 콜라츠 추측.js

--- 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.js
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -1,14 +1,14 @@
-// function solution(num) {
-//   let cnt = 0;
+function solution(num) {
+  let cnt = 0;
 
-//   while (num != 1 && cnt != 500) {
-//     num % 2 == 0 ? (num = num / 2) : (num = num * 3 + 1);
-//     cnt++;
-//   }
-//   return num == 1 ? cnt : -1;
-// }
-
-
-function solution(num,count = 0) {
-    return num == 1 ? (count >= 500 ? -1 : count) : solution(num % 2 == 0 ? num / 2 : num * 3 + 1,++count);
+  while (num != 1 && cnt != 500) {
+    num % 2 == 0 ? (num = num / 2) : (num = num * 3 + 1);
+    cnt++;
+  }
+  return num == 1 ? cnt : -1;
 }
+
+//재귀함수와 삼항연산자 다른 사람의 풀이
+// function solution(num,count = 0) {
+//     return num == 1 ? (count >= 500 ? -1 : count) : solution(num % 2 == 0 ? num / 2 : num * 3 + 1,++count);
+// }
