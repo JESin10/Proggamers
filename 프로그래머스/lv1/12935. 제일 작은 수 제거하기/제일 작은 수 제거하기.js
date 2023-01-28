608cab6aaa88f0258a1b6d6a36f456629f42d8fb
JESin10 <119720123+JESin10@users.noreply.github.com>
2023-01-28 10:31:44 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 0.05 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
@@ -0,0 +1,21 @@
+//1. 배열에서 최솟값을 찾자
+//2. 최솟값을 제거하자   ++ 2.1특정 값 제거 : 인덱스 번호 필요
+//3. 최솟값이 없다면 -1을 리턴하자.
+//4. 최솟값을 제거한 나머지 배열을 보여주자.
+
+function solution(arr) {
+    var answer = [];
+    if(arr.length<=1){
+        answer.push(-1);
+    } else {
+        let Arr = Math.min(...arr);     //1.
+        let inum = arr.indexOf(Arr);    //2.1
+        arr.splice(inum,1);             //2.
+        answer = arr;                   //3.
+    }
+   return answer;
+}
+
+
+
+
