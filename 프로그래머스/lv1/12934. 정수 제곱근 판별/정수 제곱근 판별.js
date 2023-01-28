fcaec75002c973ba47a86a36c0318d99590d5047
JESin10 <119720123+JESin10@users.noreply.github.com>
2023-01-28 09:49:21 +0900

    Update 정수 제곱근 판별.js

--- 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.js
+++ 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.js
@@ -1,25 +1,20 @@
 // function solution(n) {
 //     var answer = 0;
-//     for (let x=1;x<n;x++){
-//         if(n==x*x){
-//             answer = (x+1)*(x+1);
-//         } else{
-//             answer = -1;
-//         }
+//     answer = Math.sqrt(n);
+//     if(answer%1==0){
+//         answer = (answer+1)*(answer+1);
+//     } else{
+//         answer =-1;
 //     }
+//     //console.log(answer);
 //     return answer;
 // }
 
-
+//줄여보자
 function solution(n) {
-    var answer = 0;
-    answer = Math.sqrt(n);
-    if(answer%1==0){
-        answer = (answer+1)*(answer+1);
-    } else{
-        answer =-1;
-    }
-    //console.log(answer);
-    return answer;
+    // var answer = 0;
+    // answer = Math.sqrt(n);
+    // answer = answer%1==0? Math.pow(answer+1,2):-1;
+    
+    return Math.sqrt(n) %1 == 0 ? Math.pow(Math.sqrt(n)+1,2):-1;
 }
-
