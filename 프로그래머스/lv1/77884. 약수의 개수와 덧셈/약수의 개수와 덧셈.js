869f4eaeab024e1711c14be536ca8c25e132eb9f
JESin10 <119720123+JESin10@users.noreply.github.com>
2023-01-28 16:27:14 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.12 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
+++ 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
@@ -3,24 +3,24 @@
 // 홀수면 값을 더하고, 짝수면 빼준다
 // 반복한다
 
-function solution(left, right) {
-    var answer = 0;
-    for(let i=left ; i<=right ; i++){
-      let cnt=1; //약수는 최소 1개
-      for(let a=2; a<=i; a++){
-          if(i%a===0)cnt++; // 약수 갯수 구하기
-      }
-      cnt % 2 == 0 ? answer += i : answer -= i;  //짝수 더하고, 홀수 빼기
-    }
-    return answer;
-}
-
-
-// 제곱수를 이용하는 방법
 // function solution(left, right) {
-//   let answer = 0;
-//   for(let i=left; i<=right; i++) {
-//       Number.isInteger(Math.sqrt(i))?answer -= i:answer += i;
-//   }
-//   return answer;
+//     var answer = 0;
+//     for(let i=left ; i<=right ; i++){
+//       let cnt=1; //약수는 최소 1개
+//       for(let a=2; a<=i; a++){
+//           if(i%a===0)cnt++; // 약수 갯수 구하기
+//       }
+//       cnt % 2 == 0 ? answer += i : answer -= i;  //짝수 더하고, 홀수 빼기
+//     }
+//     return answer;
 // }
+
+
+
+function solution(left, right) {
+  let answer = 0;
+  for(let i=left; i<=right; i++) {
+      Number.isInteger(Math.sqrt(i))?answer -= i:answer += i;
+  }
+  return answer;
+}
