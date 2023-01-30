f7769752abad66ec0a640fb662c950aa32e773c5
JESin10 <119720123+JESin10@users.noreply.github.com>
2023-01-30 16:42:11 +0900

    [level 1] Title: 소수 만들기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.js
+++ 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.js
@@ -1,43 +1,47 @@
 // function solution(nums) {
-//     var answer = 0;
+//     let answer = 0;
+//     let number= 0; 
+//     let arr1 = [];
 
-//     for (let i = 0; i <nums.length-2; i ++) {
-//         for (let j = i + 1; j < nums.length-1; j++) {
-//             for (let k = j + 1; k < nums.length; k++) {
-//                 let number = (nums[i] + nums[j] + nums[k]);
-//                 if(number % 2 !== 0 && checkPrime(number)) {
-//                     answer++;
+//     for(let i=0;i<nums.length-2;i++){
+//         for(let j=i+1;j<nums.length-1;j++){
+//             for(let l=j+1;l<nums.length;l++){
+//                 let count=0;
+//                 number=nums[i]+nums[j]+nums[l];
+//                 for(let k=1;k<=number;k++){
+//                     if(number%k==0) count++;
 //                 }
+//                 (count==2)? answer++:answer;
 //             }
 //         }
 //     }
 //     return answer;
 // }
 
-// function checkPrime(target) {
-//     for (let i = 3; i <= Math.sqrt(target); i+=2) {
-//         if (target % i === 0) return false;
-//     }
-//     return true;
-// }
-
-// //반복문을 이용해 3개를 더한 숫자를 변수에 할당한다.
-// //이 수를  자연수 1과 자기자신으로 나눈 나머지가 0이 나왔을때만 카운트1을 하고 아닐때는 카운트하지 않느다.
 
 function solution(nums) {
-    let answer = 0, number=0;
-    for(let i=0;i<nums.length-2;i++){
-        for(let j=i+1;j<nums.length-1;j++){
-            for(let l=j+1;l<nums.length;l++){
-                number=nums[i]+nums[j]+nums[l];
-                let count=0;
-                for(let k=1;k<=number;k++){
-                    if(number%k==0) count++;
-                }
-                if(count==2) answer++;
-            }
-        }
+  let answer = 0;
+  let arr = [];
+  
+  for (let i = 0; i < nums.length-2; i++) {
+    for (let j = i + 1; j < nums.length- 1; j++) {
+      for (let k = j + 1; k < nums.length; k++) {
+        let result = nums[i] + nums[j] + nums[k];
+        //result가 소수냐? 아니면 answer +1
+        let cnt=0
+        for (let p = 1; p <=result; p++) {
+          if (result % p == 0) {cnt++;}
+          }
+          (cnt==2)? answer++:answer;
+        } 
+      }
     }
-    return answer;
+  return answer;
 }
+
+//  arr.sort((a, b) => a - b).filter((a, i) => a != arr[i + 1]);
+  //소수를 구하자 여기서?
+  
+
+
 
