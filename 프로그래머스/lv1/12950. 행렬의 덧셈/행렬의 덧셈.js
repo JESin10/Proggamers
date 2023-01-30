4e5175ef614c20980b32c0d3c94be5b81a1981c5
JESin10 <119720123+JESin10@users.noreply.github.com>
2023-01-30 20:14:51 +0900

    [level 1] Title: 행렬의 덧셈, Time: 7.70 ms, Memory: 67.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12950. 행렬의 덧셈/행렬의 덧셈.js
@@ -0,0 +1,16 @@
+// function solution(arr1, arr2) {
+//     var answer = [];
+
+//     for(let i=0;i<arr1.length;i++){
+//         let arr=[];
+//         for(let j=0;j<arr1[i].length;j++){
+//             arr.push(arr1[i][j]+arr2[i][j])
+//         }   
+//         answer.push(arr);
+//     }
+//     return answer;
+// }
+
+function solution(arr1, arr2) {
+   return arr1.map((a, i)=> a.map((b, j)=>b+arr2[i][j]));
+}
