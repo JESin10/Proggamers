40c40c0a43d99a1ecbad8584019c865716cd1c61
JESin10 <119720123+JESin10@users.noreply.github.com>
2023-02-01 13:15:09 +0900

    [level 1] Title: 실패율, Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/42889. 실패율/실패율.js
+++ 프로그래머스/lv1/42889. 실패율/실패율.js
@@ -1,55 +1,31 @@
-// function solution(N, stages) {
-//     var answer = [];
-//     let totalNum = stages.length;
-    
-//     for(let i=1;i<N+1;i++){
-//         let stage_n=stages.filter(c=>c==i).length
-//         let fail = 0;
-//         if(stage_n==0){
-//             fail = 0;
-//         } else {
-//             fail = stage_n/(stages.length);
-//         }
-//        stage_n.length-1;
-//         //totalNum -= stage_n;
-//         answer.push({stage:i,fail:fail});
-        
-//     }
-//     console.log(answer)
-    
-// }
-
-
 function solution(N, stages) {
     let arr = [];
-    let totalNum = stages.length;
+    let total = stages.length;
+
     for (let i=1; i<=N; i++){
-        // 현재 단계에서 실패한 유저수를 계산
-        // 현재까지의 누적 도전자수를 나누어 실패율 계산
-        // 현재까지의 누적 도전자수에 실패한 유저수 빼기
-        let stageNum = stages.filter(ele=> ele==i).length
-        let failRatio = 0;
-        if (stageNum===0){
-            failRatio = 0;
+        let stage_n = stages.filter(a=> a==i).length
+        let failratio = 0;
+        if (stage_n===0){
+            failratio = 0;
         }else{
-            failRatio = (stageNum)/totalNum;
+            failratio = (stage_n)/total;
         }
-        totalNum -= stageNum;
-        arr.push({idx:i,ratio:failRatio});
+        total -= stage_n;
+        arr.push({stage:i,failratio:failratio});
     }
-    // 실패율 내림차순으로 정렬하고 실패율이 같다면 stage 오름차순으로 정렬
+    
     arr.sort((a,b)=>{
-        if(a.ratio>b.ratio){
+        if(a.failratio>b.failratio){
             return -1;
-        }else if (a.ratio<b.ratio){
+        }else if (a.failratio<b.failratio){
             return 1;
         }else{
-            if(a.idx>b.idx){
+            if(a.stage>b.stage){
                 return 1;
             }else{
                 return -1;
             }
         }
     })
-    return arr.map(ele=>ele.idx);
+    return arr.map(c=>c.stage);
 }
