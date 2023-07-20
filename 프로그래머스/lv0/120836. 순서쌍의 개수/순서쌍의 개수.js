bd163a80e2f5349d5d951719a7cf981872a7518e
JESin10 <119720123+JESin10@users.noreply.github.com>
2023-07-20 18:36:55 +0900

    Update 순서쌍의 개수.js

--- 프로그래머스/lv0/120836. 순서쌍의 개수/순서쌍의 개수.js
+++ 프로그래머스/lv0/120836. 순서쌍의 개수/순서쌍의 개수.js
@@ -1,7 +1,16 @@
+// function solution(n) {
+// var answer = 0;
+//     for(i=1;i<=n;i++){
+//         if(n%i===0) answer++;
+//     }
+//     return answer;
+// }
+
+//제곱수이용
 function solution(n) {
-var answer = 0;
-    for(i=1;i<=n;i++){
-        if(n%i===0) answer++;
+    var answer = 0;
+    for(i=1;i*i<n;i++){
+        if(n%i==0) answer++;
     }
-    return answer;
+    return Number.isInteger(Math.sqrt(n))? (answer*2)+1:answer*2;
 }
